53d11d9e95f066ce4578ea6719d3a3174644559f
hao-to <th.nguyen77@gmail.com>
2025-05-17 19:28:20 +0200

    Add RDS security group allowing access only from EC2 instance

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -55,3 +55,30 @@ resource "aws_instance" "grocerymate_ec2" {
 output "ec2_public_ip" {
   value = aws_instance.grocerymate_ec2.public_ip
 }
+
+
+# 6 Define a security group for RDS that only allows connections from EC2
+resource "aws_security_group" "grocerymate_rds_sg" {
+  name        = "grocerymate-rds-sg"
+  description = "Allow PostgreSQL access from EC2"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Access from EC2 to PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = [aws_security_group.grocerymate_sg.id]  # <-- erlaubt Zugriff nur von EC2-SG
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "grocerymate-rds-sg"
+  }
+}
